3337aec2ab690bd23ab4fd7a81737b2eb46baf90
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-11-23 11:13:34 +0530

    Added : User choice to input character

--- com/tictactoe/TicTacToe.java
+++ com/tictactoe/TicTacToe.java
@@ -1,9 +1,48 @@
 package com.tictactoe;
 
-public class TicTacToe {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-	System.out.println("Welcome to tic Tac toe game");
+public class TicTacToe {
+        static String[] board;
+       public static void main(String args []){
+            board=new String[9];
+            EmptyBoard();
+            printBoard();
+            UserInput();
+  }
+  static void printBoard() {
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("/---|---|---\\");
 	}
 
+	static void EmptyBoard() {
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf("-");
+			
+		}
+	}
+	
+	static void UserInput() {
+		String computerSymbol=null;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your choice X or O");
+		String playerChoice = scanner.nextLine();
+		if (playerChoice == "X") {
+			computerSymbol="O";
+			playerChoice = "X";
+			System.out.println("you entered " +playerChoice +" Opponent have" +computerSymbol);
+		}
+		else if (playerChoice == "O") {
+			 computerSymbol="X";
+			 playerChoice = "O";
+			 System.out.println("you entered " +playerChoice +" Opponent have" +computerSymbol);
+		}
+		//System.out.println("you entered " +playerChoice +" Opponent have" +computerSymbol);
+		// printBoard();
+  }
 }
